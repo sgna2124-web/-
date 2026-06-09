0db4a8226d6a77afacc7cfaca13bbd880190b531
sgna2124-web <sgna2124@gmail.com>
2026-06-09 19:01:26 +0900

    Add M01 strategy spec

---
+++ base_line/short_max2/v4_highperf_M01/strategy_spec.py
@@ -0,0 +1,20 @@
+STRATEGY_NAME = 'SMX2V3_B04_M01_S02D_plus_upper020'
+PARENT = 'SMX2V2_C08_EX20_02_N02_stop257_rr5075'
+AXIS = 'short_max2'
+BRANCH = 'v4_highperf_M01'
+
+PARAMS = {
+    'short_dev': 0.032,
+    'short_rsi_min': 76.0,
+    'short_wick_mult': 1.30,
+    'score_min_short': 2.35,
+    'atr_stop_mult': 2.57,
+    'rr_mult': 5.075,
+    'timeout_bars': 320,
+    'fail_fast_bars': 12,
+    'dd_brake_trigger_pct': 0.035,
+    'dd_brake_freeze_steps': 4,
+    'soft_volr_min': 1.10,
+    'soft_volr_bonus': 0.14,
+    'soft_upper_sweep_bonus': 0.07,
+    'soft
