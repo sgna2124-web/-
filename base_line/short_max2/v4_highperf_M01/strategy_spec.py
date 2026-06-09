b276fd1afc8dbac4e9104cf545a0a6b23d5261b7
sgna2124-web <sgna2124@gmail.com>
2026-06-09 21:03:48 +0900

    Minimize M01 strategy spec index

--- base_line/short_max2/v4_highperf_M01/strategy_spec.py
+++ base_line/short_max2/v4_highperf_M01/strategy_spec.py
@@ -2,21 +2,7 @@ STRATEGY_NAME='SMX2V3_B04_M01_S02D_plus_upper020'
 PARENT='SMX2V2_C08_EX20_02_N02_stop257_rr5075'
 AXIS='short_max2'
 BRANCH='v4_highperf_M01'
-
-# core exit/risk
-atr_stop_mult=2.57
-rr_mult=5.075
-timeout_bars=320
-fail_fast_bars=12
-dd_brake_trigger_pct=0.035
-dd_brake_freeze_steps=4
-
-# core entry
-short_dev=0.032
-short_rsi_min=76.0
-short_wick_mult=1.30
-score_min_short=2.35
-
-# M01 soft modifiers
-soft_volr_min=1.10
-soft_volr_bonus=0.
+PARAM_FILE='strategy_params.csv'
+RESULT_FILE='result_summary.csv'
+PERIOD_FILE='period_summary.csv'
+SELECTION_RULE='SELECTION_RULE.md'
